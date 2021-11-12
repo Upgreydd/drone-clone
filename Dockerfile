be9aa655fe2a9a5666c05573afda9db4e78a452d
Kacper <kacper@kacperadler.info>
2021-11-12 14:04:16 +0100

    clone handling

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM alpine/git
 
-ADD clone.sh /bin/
-RUN chmod +x /bin/clone.sh
+ADD clone.sh clone-commit.sh clone-pull-request.sh clone-tag.sh /bin/
+RUN chmod +x /bin/clone.sh /bin/clone-commit.sh /bin/clone-pull-request.sh /bin/clone-tag.sh
 ENTRYPOINT /bin/clone.sh
--- clone.sh
+++ clone.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-f [[ ! -z "${DRONE_NETRC_MACHINE}" ]]; then
+if [[ -n "${DRONE_WORKSPACE}" ]]; then
+	cd ${DRONE_WORKSPACE}
+fi
+
+if [[ ! -z "${DRONE_NETRC_MACHINE}" ]]; then
 	cat <<EOF > ${HOME}/.netrc
 machine ${DRONE_NETRC_MACHINE}
 login ${DRONE_NETRC_USERNAME}
@@ -18,12 +22,50 @@ if [[ ! -z "${DRONE_SSH_KEY}" ]]; then
 	ssh-keyscan -H ${DRONE_NETRC_MACHINE} > /etc/ssh/ssh_known_hosts 2> /dev/null
 fi
 
-rm -rf ./*
+if [[ ! -z "$DRONE_AWS_ACCESS_KEY" ]]; then
+	aws configure set aws_access_key_id $DRONE_AWS_ACCESS_KEY
+	aws configure set aws_secret_access_key $DRONE_AWS_SECRET_KEY
+	aws configure set default.region $DRONE_AWS_REGION
+
+	git config --global credential.helper '!aws codecommit credential-helper $@'
+	git config --global credential.UseHttpPath true
+fi
+
+if [[ -z "${DRONE_COMMIT_AUTHOR_NAME}" ]]; then
+	export DRONE_COMMIT_AUTHOR_NAME=drone
+fi
+
+if [[ -z "${DRONE_COMMIT_AUTHOR_EMAIL}" ]]; then
+	export DRONE_COMMIT_AUTHOR_EMAIL=drone@localhost
+fi
+
+export GIT_AUTHOR_NAME=${DRONE_COMMIT_AUTHOR_NAME}
+export GIT_AUTHOR_EMAIL=${DRONE_COMMIT_AUTHOR_EMAIL}
+export GIT_COMMITTER_NAME=${DRONE_COMMIT_AUTHOR_NAME}
+export GIT_COMMITTER_EMAIL=${DRONE_COMMIT_AUTHOR_EMAIL}
+
+CLONE_TYPE=$DRONE_BUILD_EVENT
+case $DRONE_COMMIT_REF in
+  refs/tags/* ) CLONE_TYPE=tag ;;
+  refs/pull/* ) CLONE_TYPE=pull_request ;;
+  refs/pull-request/* ) CLONE_TYPE=pull_request ;;
+  refs/merge-requests/* ) CLONE_TYPE=pull_request ;;
+esac
+
 if [[ -n "$PLUGIN_REMOTE" ]] && [[ -n "$PLUGIN_VERSION" ]] ; then
-  git clone --single-branch --branch "${VERSION:-${DRONE_BRANCH}}" https://github.com/"${REMOTE:-${DRONE_REPO_NAMESPACE}}"/"${DRONE_REPO_NAME}".git .
+  echo "Using remote: $PLUGIN_REMOTE"
+  echo "Using version: $PLUGIN_VERSION"
+  git clone --single-branch --branch "${PLUGIN_VERSION:-${DRONE_BRANCH}}" https://github.com/"${PLUGIN_REMOTE:-${DRONE_REPO_NAMESPACE}}"/"${DRONE_REPO_NAME}".git .
 else
-  git clone -b "$DRONE_BRANCH" "$DRONE_REMOTE_URL" .
-  git checkout "$DRONE_BRANCH"
-  git fetch origin "$DRONE_COMMIT_REF"
-  git merge "$DRONE_COMMIT"
+  case $CLONE_TYPE in
+  pull_request)
+    clone-pull-request.sh
+    ;;
+  tag)
+    clone-tag.sh
+    ;;
+  *)
+    clone-commit.sh
+    ;;
+  esac
 fi
