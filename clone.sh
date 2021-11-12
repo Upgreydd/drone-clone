c34d509f61061d48d26a15f1ec76db59943aea64
Kacper <kacper@kacperadler.info>
2021-11-12 13:54:13 +0100

    Add netrc and ssh key

--- clone.sh
+++ clone.sh
@@ -1,4 +1,23 @@
 #!/bin/sh
+
+f [[ ! -z "${DRONE_NETRC_MACHINE}" ]]; then
+	cat <<EOF > ${HOME}/.netrc
+machine ${DRONE_NETRC_MACHINE}
+login ${DRONE_NETRC_USERNAME}
+password ${DRONE_NETRC_PASSWORD}
+EOF
+fi
+
+if [[ ! -z "${DRONE_SSH_KEY}" ]]; then
+	mkdir ${HOME}/.ssh
+	echo -n "$DRONE_SSH_KEY" > ${HOME}/.ssh/id_rsa
+	chmod 600 ${HOME}/.ssh/id_rsa
+
+	touch ${HOME}/.ssh/known_hosts
+	chmod 600 ${HOME}/.ssh/known_hosts
+	ssh-keyscan -H ${DRONE_NETRC_MACHINE} > /etc/ssh/ssh_known_hosts 2> /dev/null
+fi
+
 rm -rf ./*
 if [[ -n "$PLUGIN_REMOTE" ]] && [[ -n "$PLUGIN_VERSION" ]] ; then
   git clone --single-branch --branch "${VERSION:-${DRONE_BRANCH}}" https://github.com/"${REMOTE:-${DRONE_REPO_NAMESPACE}}"/"${DRONE_REPO_NAME}".git .
